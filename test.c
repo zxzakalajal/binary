09e2d86980a9eb4b3f3f59c20f27ff59dfeb0d60
zxzakalajal <zxzakalajal85@gmail.com>
2021-03-12 19:48:40 +0900

    2진수로 변환하는 두번째 함수 작성

--- test.c
+++ test.c
@@ -6,3 +6,31 @@ void DecToBinary(int n){
 		printf("%d",n%2);
 	}
 }
+
+void Binary(int n){
+	
+	unsigned i = 1 << 31;
+	int check = 0;
+	while (i)
+	{
+		if(i & n)
+		{
+			printf("1");
+			check = 1;
+		}
+		else 
+		{
+			if(check !=0)
+			{
+				printf("0");
+			}
+		}
+		i = n >> 1;
+	}
+}
+int main(void){
+	DecToBinary(10);
+	printf("\n");
+	Binary(12);
+	print("\n");
+}
